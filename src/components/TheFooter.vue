c64600f273cdc700fd9ff1fa8b496599bf43a555
0xfe <2953427626@qq.com>
2025-05-20 18:29:17 +0800

    fix: update GitHub link in TheFooter component

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -7,7 +7,7 @@
     <a
       i-carbon-logo-github icon-btn
       rel="noreferrer"
-      href="https://github.com/antfu-collective/vitesse-lite"
+      href="https://github.com/Gianthard-cyh/riscv-pte"
       target="_blank"
       title="GitHub"
     />
